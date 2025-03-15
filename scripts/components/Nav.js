ab331f14c4af484b08fe995246d0f763985c3dd5
Eran Godsi <eran.godsi2003@gmail.com>
2025-03-15 20:39:07 +0200

    Fix mistake in Nav links

--- scripts/components/Nav.js
+++ scripts/components/Nav.js
@@ -4,8 +4,8 @@
 export const Nav = {
     template: `
         <navbar>
-            <a href="/#/plans">Plans</a>
-            <a href="/#/logs">Logs</a>
+            <a href="#/plans">Plans</a>
+            <a href="#/logs">Logs</a>
         </navbar>
     `
 }
